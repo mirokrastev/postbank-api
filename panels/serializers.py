a9b68f20c98721fe13c1afc8cbb89e628f6f978f
spsudspsp <68426394+Spsudspsp@users.noreply.github.com>
2022-06-18 12:10:06 +0300

    added trader serializer to clients endpoint

--- panels/serializers.py
+++ panels/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
+from accounts.serializers import TraderSerializer
 from panels import models
 
 
 class DiscountSerializer(serializers.ModelSerializer):
+    trader = TraderSerializer()
+
     class Meta:
         model = models.Discount
         fields = '__all__'
