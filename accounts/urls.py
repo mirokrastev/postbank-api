805ca919f0ca18ce0ca336d83c411ddc43e79a0e
mirokrastev <mirokrastev@protonmail.com>
2022-06-18 16:55:52 +0300

    add type column to users, refactor serializers

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,12 +8,8 @@ app_name = 'accounts'
 auth_router = routers.DefaultRouter()
 auth_router.register('register', views.RegisterViewSet, basename='register')
 
-router = routers.DefaultRouter()
-router.register('users', views.UserViewSet, basename='users')
-
 urlpatterns = [
     path('api/auth/login/', views.LoginView.as_view(), name='login'),
     path('api/auth/logout/', views.LogoutView.as_view(), name='logout'),
     re_path('^api/auth/', include(auth_router.urls)),
-    re_path('^api/', include(router.urls)),
 ]
